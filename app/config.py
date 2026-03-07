caefde0aec9c23fe878ac84f19ca0f93dc702b40
m.kosek <m.kosek@netsystem.cz>
2026-03-07 17:43:09 +0100

    added logging

--- app/config.py
+++ app/config.py
@@ -6,9 +6,15 @@ load_dotenv()
 
 with open("config.json", "r") as f:
     config_data = json.load(f)
+    log_config = config_data.get("logging", {})
 
 class Config:
     API_URL = config_data.get("api_url")
+
+    LOG_LEVEL = log_config.get("level", "INFO")
+    LOG_FILENAME = log_config.get("filename", "logs/app.log")
+    LOG_MAX_BYTES = log_config.get("max_bytes", 5242880)
+    LOG_BACKUP_COUNT = log_config.get("backup_count", 3)
     
     ADMIN_USERNAME = os.getenv("ADMIN_USERNAME")
     ADMIN_PASSWORD = os.getenv("ADMIN_PASSWORD")
