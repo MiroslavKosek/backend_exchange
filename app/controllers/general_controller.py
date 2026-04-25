8fc46005c743f430d425ed4939e30b7ef717db35
m.kosek <m.kosek@netsystem.cz>
2026-04-26 00:07:22 +0200

    added endpoint for client logs

--- app/controllers/general_controller.py
+++ app/controllers/general_controller.py
@@ -2,6 +2,7 @@
 
 from fastapi import APIRouter
 
+from app.client_log import ClientLog
 from app.logger import logger
 
 router = APIRouter(tags=["General"])
@@ -20,6 +21,25 @@ async def root():
     logger.debug(f"Root endpoint returning payload: {response_payload}")
     return response_payload
 
+@router.post("/api/logs")
+async def log_message(log_data: ClientLog):
+    """Endpoint to receive log messages from the Angular frontend."""
+
+    frontend_msg = (
+        f"[Frontend -> {log_data.fileName}:{log_data.lineNumber}] "
+        f"{log_data.message} | Extras: {log_data.additional}"
+    )
+
+    if log_data.level >= 5:
+        logger.error(frontend_msg)
+    elif log_data.level == 4:
+        logger.warning(frontend_msg)
+    elif log_data.level in [2, 3]:
+        logger.info(frontend_msg)
+    else:
+        logger.debug(frontend_msg)
+
+    return {"status": "logged"}
 
 @router.get("/health")
 async def health_check():
